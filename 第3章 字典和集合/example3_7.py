0d776916a5d35d8db031e6c9d28a2a3e143fde1e
zhonghui.yao <zhonghua00700@gmail.com>
2019-12-13 00:52:25 +0800

    chapter 3 -1

---
+++ 第3章 字典和集合/example3_7.py
@@ -0,0 +1,17 @@
+class StrKeyDict(dict):
+
+    def __missing__(self, key):
+        if isinstance(key, str):
+            raise KeyError
+        else:
+            return self[str(key)]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __contains__(self, key):
+        return key in self.keys or str(key) in self.keys()
+
