9112227bee604be302cb1d486996331146342d87
zhonghui.yao <zhonghua00700@gmail.com>
2019-12-27 20:59:37 +0800

    change fibonacci

--- 第14章 可迭代的对象、迭代器和生成器/fibonacci.py
+++ 第14章 可迭代的对象、迭代器和生成器/fibonacci.py
@@ -1,3 +1,8 @@
+# 1. 使用生成器生成斐波那契数列
+import heapq
+import numbers
+
+
 def fibonacci():
     a, b = 0, 1
     while True:
@@ -11,3 +16,32 @@ print(f, type(f))
 
 for i in range(6):
     print(next(f))
+
+print('=============================================================')
+# 2. 使用递归+缓存
+import functools
+
+
+@functools.lru_cache()
+def fibonacci_1(n):
+    if n < 2:
+        return n
+    return fibonacci_1(n-2) + fibonacci_1(n-1)
+
+print('fibonacci_1', fibonacci_1(6))
+
+
+print('=============================================================')
+# 3. 使用循环
+
+def fibonacci_2(n):
+    a, b = 0, 1
+    while n > 0 and isinstance(n, numbers.Integral):
+        a, b = b, a + b
+        n -= 1
+
+    return a
+
+print('fibonacci_2', fibonacci_2(6))
+
+print('=============================================================')
